a6b7947109cc732556fdae3338198e873f3d2c6e
Jonas Hammerschmidt <j.hammerschmidt@krz.de>
2022-02-25 19:53:33 +0100

    Improved solution

--- MultipleSalesman/Calculations/Algorithmus2.cs
+++ MultipleSalesman/Calculations/Algorithmus2.cs
@@ -4,11 +4,9 @@
     {
         private readonly Random random = new Random();
 
-        private Point[] routeCopy = new Point[0];
-        
         public Point[] Rechne(Point[] route, int plaetze)
         {
-            routeCopy = new Point[route.Length];
+            Point[] routeCopy = new Point[route.Length];
 
             Array.Copy(route, routeCopy, route.Length);
             routeCopy = SwapRoutePoints(routeCopy, plaetze);
@@ -25,11 +23,7 @@
             {
                 swapIndex1 = this.random.Next() % route.Length;
                 swapIndex2 = this.random.Next() % route.Length;
-            } while (
-                swapIndex1 % (plaetze + 1) == 0 ||
-                swapIndex2 % (plaetze + 1) == 0 ||
-                swapIndex1 == route.Length - 1 ||
-                swapIndex2 == route.Length - 1);
+            } while (!IsValidSwap(route, plaetze, swapIndex1, swapIndex2));
 
             var tmp = route[swapIndex2];
             route[swapIndex2] = route[swapIndex1];
@@ -38,5 +32,40 @@
             return route;
         }
 
+        private bool IsValidSwap(Point[] route, int plaetze, int swapIndex1, int swapIndex2)
+        {
+            if (swapIndex1 == 0 || swapIndex2 == 0 || swapIndex1 == route.Length - 1 || swapIndex2 == route.Length - 1)
+            {
+                return false;
+            }
+
+            Point startingPoint = route[0];
+
+            Point[] routeCopy2 = new Point[route.Length];
+            Array.Copy(route, routeCopy2, route.Length);
+            var tmp = routeCopy2[swapIndex2];
+            routeCopy2[swapIndex2] = routeCopy2[swapIndex1];
+            routeCopy2[swapIndex1] = tmp;
+
+            int usedPlaetze = 0;
+            foreach (var stop in routeCopy2)
+            {
+                if (stop.X != startingPoint.X || stop.Y != startingPoint.Y)
+                {
+                    usedPlaetze++;
+
+                    if (usedPlaetze > plaetze)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    usedPlaetze = 0;
+                }
+            }
+
+            return true;
+        }
     }
 }
--- MultipleSalesman/Form1.cs
+++ MultipleSalesman/Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace MultipleSalesman
 {
     public partial class Form1 : Form
@@ -6,7 +8,7 @@ namespace MultipleSalesman
             new Point(80, 120),
             new Point(150, 150),
             new Point(60, 220),
-            new Point(140, 350),
+            new Point(590, 160),
             new Point(220, 420),
             new Point(170, 470),
             new Point(250, 450),
@@ -16,6 +18,19 @@ namespace MultipleSalesman
             new Point(640, 300),
             new Point(620, 250),
             new Point(520, 60),
+            new Point(10, 210),
+            new Point(510, 510),
+            new Point(610, 220),
+            new Point(350, 610),
+            new Point(220, 240),
+            new Point(210, 340),
+            new Point(520, 540),
+            new Point(330, 550),
+            new Point(240, 060),
+            new Point(530, 460),
+            new Point(460, 030),
+            new Point(260, 520),
+            new Point(250, 06),
         };
 
         private Point startingPoint = new Point(420, 340);
@@ -27,27 +42,42 @@ namespace MultipleSalesman
         private int iteration = 0;
         private int bestIteration = 0;
 
+        private bool isCancled = false;
+
         public Form1()
         {
             InitializeComponent();
 
             map = Image.FromFile("map.png");
+            StartAlgorithmus();
+        }
 
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            isCancled = true;
+            Thread.Sleep(1000);
+            isCancled = false;
+            iteration = 0;
+            bestIteration = 0;
+            this.StartAlgorithmus();
+        }
+
+        private void StartAlgorithmus()
+        {
             Algorithmus algorithmus = new Algorithmus();
 
             var besteRoute = algorithmus.Rechne(route, Plaetze, startingPoint);
+            this.RenderRoute(besteRoute, Plaetze);
             Task.Run(async () =>
             {
                 await Task.Delay(100);
-                while (true)
+                while (!isCancled)
                 {
-                    if (iteration > 1000000) {
-                        iteration = 0;
-                        bestIteration = 0;
-                        besteRoute = algorithmus.Rechne(route, Plaetze, startingPoint);
+                    if (iteration % 800 == 0)
+                    {
+                        await Task.Delay(100);
+                        this.RenderRoute(besteRoute, Plaetze);
                     }
-
-                    //await Task.Delay(100);
                     Algorithmus2 algorithmus2 = new Algorithmus2();
                     var bessereRoute = algorithmus2.Rechne(besteRoute, Plaetze);
                     iteration++;
@@ -55,29 +85,30 @@ namespace MultipleSalesman
                     {
                         besteRoute = bessereRoute;
                         bestIteration = iteration;
-                        this.RenderRoute(besteRoute, Plaetze);
                     }
                 }
             });
 
-            Task.Run(async () =>
-            {
-                while (true)
-                {
-                    await Task.Delay(1000);
-                    Console.WriteLine("Iteration: " + iteration.ToString());
-                }
-            });
+            //Task.Run(async () =>
+            //{
+            //    while (!isCancled)
+            //    {
+            //        await Task.Delay(500);
+            //        this.RenderRoute(besteRoute, Plaetze);
+            //    }
+            //});
         }
-
 
         private void RenderRoute(Point[] route, int plaetze)
         {
-            Pen blackPen = new Pen(Color.Black);
-            blackPen.Width = 3;
+            Pen pen = new Pen(Color.Black);
+            pen.Width = 3;
+            Pen penBigger = new Pen(Color.Black);
+            penBigger.Width = 5;
             SolidBrush whiteBrush = new SolidBrush(Color.White);
             SolidBrush blackBrush = new SolidBrush(Color.Black);
 
+            int trip = 0;
             using (Graphics myCanvas = pictureBox1.CreateGraphics())
             {
                 myCanvas.DrawImage(map, 0 , 0);
@@ -88,8 +119,14 @@ namespace MultipleSalesman
                     if (i < route.Length - 1)
                     {
                         Point nextPoint = route[i + 1];
-                        blackPen.Color = ColorScale.ColorFromHSL((i / (plaetze + 1)) * 0.3f, 1, 0.5);
-                        myCanvas.DrawLine(blackPen, point.X, point.Y, nextPoint.X, nextPoint.Y);
+
+                        if (point == startingPoint)
+                        {
+                            pen.Color = ColorScale.ColorFromHSL(new Random(trip+=5).NextSingle(), 1, 0.5);
+                        }
+
+                        myCanvas.DrawLine(penBigger, point.X, point.Y, nextPoint.X, nextPoint.Y);
+                        myCanvas.DrawLine(pen, point.X, point.Y, nextPoint.X, nextPoint.Y);
                     }
 
                     myCanvas.FillEllipse(whiteBrush, point.X - 10, point.Y - 10, 20, 20);
