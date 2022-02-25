e4012404681737819381b141a02600e58564b29e
Jonas Hammerschmidt <j.hammerschmidt@krz.de>
2022-02-25 15:54:32 +0100

    Improved rendering

--- MultipleSalesman/Calculations/Algorithmus.cs
+++ MultipleSalesman/Calculations/Algorithmus.cs
@@ -1,15 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MultipleSalesman
+﻿namespace MultipleSalesman
 {
     internal class Algorithmus
     {
-        public Point[] Rechne(Point[] route) {
-            return route;
+        public Point[] Rechne(Point[] route, int plaetze, Point startingPoint)
+        {
+            var route2 = GetRouteWithReturnToStartingPoint(route, plaetze, startingPoint);
+
+
+
+            return route2.ToArray();
+        }
+
+        private List<Point> GetRouteWithReturnToStartingPoint(Point[] route, int plaetze, Point startingPoint)
+        {
+            List<Point> routeWithStops = new List<Point>();
+
+            for (int i = 0; i < route.Length; i++)
+            {
+                if (i % plaetze == 0)
+                {
+                    routeWithStops.Add(startingPoint);
+                }
+
+                routeWithStops.Add(route[i]);
+            }
+            routeWithStops.Add(startingPoint);
+
+            return routeWithStops;
         }
     }
 }
--- MultipleSalesman/Form1.cs
+++ MultipleSalesman/Form1.cs
@@ -4,11 +4,13 @@ namespace MultipleSalesman
     {
 
         Point[] route = new Point[] {
-            new Point(50, 10),
             new Point(170, 50),
-            new Point(250, 150),
+            new Point(270, 180),
+            new Point(160, 10),
+            new Point(50, 10),
+            new Point(250, 130),
+            new Point(180, 30),
             new Point(50, 210),
-            new Point(180, 130),
         };
 
         public Form1()
@@ -19,7 +21,7 @@ namespace MultipleSalesman
             {
                 Thread.Sleep(1000);
                 Algorithmus algorithmus = new Algorithmus();
-                var bessereRoute = algorithmus.Rechne(route);
+                var bessereRoute = algorithmus.Rechne(route, 3, new Point(150, 150));
                 this.RenderRoute(bessereRoute);
             });
         }
@@ -29,6 +31,7 @@ namespace MultipleSalesman
         {
             Pen whitePen = new Pen(Color.White);
             SolidBrush whiteBrush = new SolidBrush(Color.White);
+            SolidBrush blackBrush = new SolidBrush(Color.Black);
 
             var centerX = route.Average(point => point.X);
             var centerY = route.Average(point => point.Y);
@@ -42,13 +45,16 @@ namespace MultipleSalesman
                 for (int i = 0; i < route.Length; i++)
                 {
                     Point point = route[i];
-                    myCanvas.FillEllipse(whiteBrush, point.X - 5 + offsetX, point.Y - 5 + offsetY, 10, 10);
 
                     if (i < route.Length - 1)
                     {
                         Point nextPoint = route[i + 1];
+                        whitePen.Color = ColorScale.ColorFromHSL((float)i / route.Length, 1, 0.5);
                         myCanvas.DrawLine(whitePen, point.X + offsetX, point.Y + offsetY, nextPoint.X + offsetX, nextPoint.Y + offsetY);
                     }
+
+                    myCanvas.FillEllipse(whiteBrush, point.X - 10 + offsetX, point.Y - 10 + offsetY, 20, 20);
+                    myCanvas.DrawString(i.ToString(), new Font("Comic Sans MS", 14, FontStyle.Bold), blackBrush, point.X - 10 + offsetX, point.Y - 10 - 4 + offsetY);
                 }
             }
         }
